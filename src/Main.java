d9a2137bb61c5a98fb3d8c548bce5a20d3b331c5
NOHKYEONGMIN <sji4844@naver.com>
2026-04-06 12:17:15 +0900

    Mission 02 - 아기사자 명단 관리 프로그램

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Final Commit!");
-        System.out.println("hello!");
-        System.out.println("Feature branch!");
-        System.out.println("asaxddddaadsadddasdsddsadbc");
+        Scanner sc = new Scanner(System.in);
+
+        int count;
+
+        while (true) {
+            System.out.print("아기사자 인원 수를 입력하세요(최소 5명): ");
+            count = sc.nextInt();
+            sc.nextLine();
+
+            if (count >= 5) {
+                break;
+            } else {
+                System.out.println("5명 미만은 입력할 수 없습니다. 다시 입력해주세요.");
+            }
+        }
+
+        String[] names = new String[count];
+
+        for (int i = 0; i < count; i++) {
+            System.out.print((i + 1) + "번째 이름 입력: ");
+            names[i] = sc.nextLine();
+        }
+
+        System.out.println("\n최종 아기사자 명단");
+        for (int i = 0; i < names.length; i++) {
+            System.out.println((i + 1) + ". " + names[i]);
+        }
+
+        sc.close();
     }
 }
